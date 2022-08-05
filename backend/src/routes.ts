60a5b09c1df7630173ae5ecbfa2cd4408955dd8f
Jorge Gurgel <jlgurgelfernandes@gmail.com>
2022-08-05 13:13:07 -0300

    adc item in order

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -11,6 +11,7 @@ import uploadConfig from "./config/multer";
 import { ListByCategoryController } from "./controllers/product/ListByCategoryController";
 import { CreateOrderController } from "./controllers/order/CreateOrderController";
 import { RemoveOrderController } from "./controllers/order/RemoveOrderController";
+import { AddItemController } from "./controllers/order/AddItemController";
 const router = Router();
 
 const upload = multer(uploadConfig.upload("./tmp"));
@@ -45,5 +46,5 @@ router.get(
 //ORDER router
 router.post("/order", isAuthenticated, new CreateOrderController().handle);
 router.delete("/order", isAuthenticated, new RemoveOrderController().handle);
-
+router.post("/order/add", isAuthenticated, new AddItemController().handle);
 export { router };
