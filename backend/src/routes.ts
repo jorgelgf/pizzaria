862878ca2a5610408f514db42ac2a77c114fb72c
Jorge Gurgel <jlgurgelfernandes@gmail.com>
2022-08-08 11:18:25 -0300

    remove item

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -12,6 +12,7 @@ import { ListByCategoryController } from "./controllers/product/ListByCategoryCo
 import { CreateOrderController } from "./controllers/order/CreateOrderController";
 import { RemoveOrderController } from "./controllers/order/RemoveOrderController";
 import { AddItemController } from "./controllers/order/AddItemController";
+import { RemoveItemController } from "./controllers/order/RemoveItemController";
 const router = Router();
 
 const upload = multer(uploadConfig.upload("./tmp"));
@@ -46,5 +47,11 @@ router.get(
 //ORDER router
 router.post("/order", isAuthenticated, new CreateOrderController().handle);
 router.delete("/order", isAuthenticated, new RemoveOrderController().handle);
+//ADD and RM item
 router.post("/order/add", isAuthenticated, new AddItemController().handle);
+router.delete(
+  "/order/remove",
+  isAuthenticated,
+  new RemoveItemController().handle
+);
 export { router };
