aba991144f4bbcfd29a58d1dd523e4b4de22755a
Jorge Gurgel <jlgurgelfernandes@gmail.com>
2022-08-17 12:25:07 -0300

    creating login page

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,8 +1,28 @@
+import Head from "next/head";
+import Image from "next/image";
 import styles from "../../styles/Home.module.scss";
+import logoImg from "../../public/logo.svg";
+import { Input } from "../components/ui/input";
+import { Button } from "../components/ui/Button";
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <h1>Sujeito pizza</h1>
-    </div>
+    <>
+      <Head>
+        <title>SujeitoPizza - Faça seu login</title>
+      </Head>
+      <div className={styles.containerCenter}>
+        <Image src={logoImg} alt="Logo Sujeito Pizzaria" />
+        <div className={styles.login}>
+          <form>
+            <Input placeholder="Digite seu e-mail" type="text" />
+            <Input placeholder="Digite sua senha" type="password" />
+
+            <Button type="submit" loading={false}>
+              Cadastrar
+            </Button>
+          </form>
+        </div>
+      </div>
+    </>
   );
 }
