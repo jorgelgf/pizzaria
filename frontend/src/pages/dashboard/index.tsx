4980f7d0881bc56fea45a8cf261ac2cfef335f92
Jorge Gurgel <jlgurgelfernandes@gmail.com>
2022-08-30 10:57:27 -0300

    create header

--- frontend/src/pages/dashboard/index.tsx
+++ frontend/src/pages/dashboard/index.tsx
@@ -1,10 +1,17 @@
 import { canSSRAuth } from "../../utils/canSSRAuth";
-
+import Head from "next/head";
+import { Header } from "../../components/Header";
 export default function Dashboard() {
   return (
-    <div>
-      <h1>Welcome panel </h1>
-    </div>
+    <>
+      <Head>
+        <title>Painel - Sujeito Pizzaria</title>
+      </Head>
+      <div>
+        <Header />
+        <h1>Painel</h1>
+      </div>
+    </>
   );
 }
 
