b95d292b67dafc5bb8c1873a637277d8c4696aad
Jorge Gurgel <jlgurgelfernandes@gmail.com>
2022-09-02 12:00:46 -0300

    Orders list

--- frontend/src/pages/dashboard/index.tsx
+++ frontend/src/pages/dashboard/index.tsx
@@ -1,7 +1,28 @@
 import { canSSRAuth } from "../../utils/canSSRAuth";
 import Head from "next/head";
 import { Header } from "../../components/Header";
-export default function Dashboard() {
+import styles from "./styles.module.scss";
+import { FiRefreshCcw } from "react-icons/fi";
+import { setupAPIClient } from "../../services/api";
+import { useState } from "react";
+
+type OrdersProps = {
+  id: string;
+  table: string | number;
+  status: boolean;
+  draft: boolean;
+  name: string | null;
+};
+
+interface HomeProps {
+  orders: OrdersProps[];
+}
+export default function Dashboard({ orders }: HomeProps) {
+  const [orderList, setOrderList] = useState(orders || []);
+  const handleOpenModalView = (id: string) => {
+    return alert("ID clicado: " + id);
+  };
+
   return (
     <>
       <Head>
@@ -9,14 +30,37 @@ export default function Dashboard() {
       </Head>
       <div>
         <Header />
-        <h1>Painel</h1>
+        <main className={styles.container}>
+          <div className={styles.containerHeader}>
+            <h1>Últimos pedidos</h1>
+            <button>
+              <FiRefreshCcw size={25} color="#3fffa3" />
+            </button>
+          </div>
+          <article className={styles.listOrders}>
+            {orderList.map((item) => (
+              <section key={item.id} className={styles.orderItem}>
+                <button onClick={() => handleOpenModalView(item.id)}>
+                  <div className={styles.tag}></div>
+                  <span>Mesa {item.table}</span>
+                </button>
+              </section>
+            ))}
+          </article>
+        </main>
       </div>
     </>
   );
 }
 
 export const getServerSideProps = canSSRAuth(async (ctx) => {
+  const apiClient = setupAPIClient(ctx);
+  const response = await apiClient.get("/orders");
+  //console.log(response.data);
+
   return {
-    props: {},
+    props: {
+      orders: response.data,
+    },
   };
 });
